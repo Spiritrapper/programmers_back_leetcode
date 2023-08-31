c03cc98fb149b2403d9eb39f3eba86c8b7b0d7ad
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-08-31 20:31:48 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		int s_num = s.length();
+		
+		boolean is_symmetric = true;
+		
+		for(int i=0; i<(s_num / 2); i++) {
+			if (s.charAt(i) != s.charAt(s_num -i-1)) {
+				is_symmetric = false;
+				break;
+			}
+		}
+		
+		if(is_symmetric) {
+			System.out.println("1");
+		}
+		else {
+			System.out.println("0");
+		}
+
+		
+	}
+}
