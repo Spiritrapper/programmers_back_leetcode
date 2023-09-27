2487cd5a110978dcac64cf45bf0680f520cc7c09
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-09-28 07:31:49 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = {};
+        for(int i=1; i<=n; i++){
+            if(n%i=0 && m=0 && i !=l){
+                answer +=i;
+            }
+        }
+        while(true){
+            
+        }
+            if()
+        }
+        return answer;
+    }
+}
