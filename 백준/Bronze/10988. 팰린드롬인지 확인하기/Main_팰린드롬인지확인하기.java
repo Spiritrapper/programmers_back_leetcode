53738d4f2b4bc425cc4a41590a05dcac0c7490a6
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2024-08-03 14:31:27 +0900

    study

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/Main_팰린드롬인지확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/Main_팰린드롬인지확인하기.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
 
-public class Main {
+public class Main_팰린드롬인지확인하기 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
